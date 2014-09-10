10a3bfa6d79801959e870fead27f48e8865f9814
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2014-09-10 19:29:52 +0100

    101 tweaks

--- OneOhOneTalks.hs
+++ OneOhOneTalks.hs
@@ -30,7 +30,7 @@ talks = zipWith (,) [(0::Int)..] $ reverse [
   speakerurl = "http://www.cl.cam.ac.uk/~ok259/",
   insturl    = "http://www.cl.cam.ac.uk/",
   title      = "Graphical algebraic foundations for monad stacks",
-  abstract   = "<p>Ohad gave an informal overview of his current draft, with the following abstract:</p><p>Haskell incorporates computational effects modularly using sequences of monad transformers, termed monad stacks. The current practice is to find the appropriate stack for a given task using intractable brute force and heuristics. By restricting attention to algebraic stack combinations, we provide a linear-time algorithm for generating all the appropriate monad stacks, or decide no such stacks exist. Our approach is based on Hyland, Plotkin, and Power's algebraic analysis of monad transformers, who propose a graph-theoretical solution to this problem. We extend their analysis with a straightforward connection to the modular decomposition of a graph and to cographs, a.k.a. series-parallel graphs.</p>\n<p>We present an accessible and self-contained account of this monad-stack generation problem, and, more generally, of the decomposition of a combined algebraic theory into sums and tensors, and its algorithmic solution. We provide a web-tool implementing this algorithm intended for semantic investigations of effect combinations and for monad stack generation.</p>",
+  abstract   = "Ohad gave an informal overview of his current draft, with the following abstract:\n\nHaskell incorporates computational effects modularly using sequences of monad transformers, termed monad stacks. The current practice is to find the appropriate stack for a given task using intractable brute force and heuristics. By restricting attention to algebraic stack combinations, we provide a linear-time algorithm for generating all the appropriate monad stacks, or decide no such stacks exist. Our approach is based on Hyland, Plotkin, and Power's algebraic analysis of monad transformers, who propose a graph-theoretical solution to this problem. We extend their analysis with a straightforward connection to the modular decomposition of a graph and to cographs, a.k.a. series-parallel graphs.\n\nWe present an accessible and self-contained account of this monad-stack generation problem, and, more generally, of the decomposition of a combined algebraic theory into sums and tensors, and its algorithmic solution. We provide a web-tool implementing this algorithm intended for semantic investigations of effect combinations and for monad stack generation.",
   location   = "LT1310",
   material   = [] }
  ,
@@ -56,7 +56,7 @@ talks = zipWith (,) [(0::Int)..] $ reverse [
   speakerurl = "https://gallais.org",
   insturl    = "",
   title      = "Resource aware contexts and proof search for IMLL",
-  abstract   = "<p>In Intuitionistic Multiplicative Linear Logic, the right introduction rule for tensors implies picking a 2-partition of the set of assumptions and use each component to inhabit the corresponding tensor's subformulas. This makes a naïve proof search algorithm intractable. Building a notion of resource availability in the context and massaging the calculus into a more general one handling both resource consumption and a notion of \"leftovers\" of a subproof allows for a well-structured well-typed by construction proof search mechanism.</p>\n  <p>Here is an <a href='http://gallais.org/code/LinearProofSearch/poc.LinearProofSearch.html'>Agda file</a> implementing the proof search algorithm.</p>",
+  abstract   = "In Intuitionistic Multiplicative Linear Logic, the right introduction rule for tensors implies picking a 2-partition of the set of assumptions and use each component to inhabit the corresponding tensor's subformulas. This makes a naïve proof search algorithm intractable. Building a notion of resource availability in the context and massaging the calculus into a more general one handling both resource consumption and a notion of \"leftovers\" of a subproof allows for a well-structured well-typed by construction proof search mechanism.\n\n Here is an <a href='http://gallais.org/code/LinearProofSearch/poc.LinearProofSearch.html'>Agda file</a> implementing the proof search algorithm.",
   location   = "LT1310",
   material   = [] }
  ,
@@ -66,7 +66,7 @@ talks = zipWith (,) [(0::Int)..] $ reverse [
                         (timeOfDayToTime (TimeOfDay 11 0 0))),
   speaker    = "Jamie Gabbay",
   institute  = "Heriot-Watt",
-  speakerurl = "https://gabbay.org.uk",
+  speakerurl = "http://www.gabbay.org.uk",
   insturl    = "http://www.macs.hw.ac.uk/departments/computer-science.htm",
   title      = "Nominal sets",
   abstract   = "",
@@ -212,7 +212,7 @@ talks = zipWith (,) [(0::Int)..] $ reverse [
   speakerurl = "http://www.cs.rhul.ac.uk/~robin/",
   insturl    = "",
   title      = "Classical Type Theories",
-  abstract   = "<p>In 1987, Felleisen showed how to add control operators (for things like exceptions and unconditional jumps) to the untyped lambda-calculus.  In 1990, Griffin idly wondered what would happen if one did the same in a typed lambda calculus.  The answer came out: the inhabited types become the theorems of classical logic.</p>\n<p>I will present the lambda mu-calculus, one of the cleanest attempts to add control operators to a type theory.  We'll cover the good news: the inhabited types are the tautologies of minimal classical logic, and Godel's Double Negation translation from classical to intuitionistic logic turns into the CPS translation.</p>\n<p>And the bad news: control operators don't play well with other types. Add natural numbers (or some other inductive type), and you get inconsistency. Add Sigma-types, and you get degeneracy (any two objects of the same type are definitionally equal).  It gets worse: add plus-types, and you break Subject Reduction.",
+  abstract   = "In 1987, Felleisen showed how to add control operators (for things like exceptions and unconditional jumps) to the untyped lambda-calculus.  In 1990, Griffin idly wondered what would happen if one did the same in a typed lambda calculus.  The answer came out: the inhabited types become the theorems of classical logic.\n\n I will present the lambda mu-calculus, one of the cleanest attempts to add control operators to a type theory.  We'll cover the good news: the inhabited types are the tautologies of minimal classical logic, and Godel's Double Negation translation from classical to intuitionistic logic turns into the CPS translation.\n\nAnd the bad news: control operators don't play well with other types. Add natural numbers (or some other inductive type), and you get inconsistency. Add Sigma-types, and you get degeneracy (any two objects of the same type are definitionally equal).  It gets worse: add plus-types, and you break Subject Reduction.",
   location   = "LT1310",
   material   = [] }
  ,
@@ -226,15 +226,15 @@ talks = zipWith (,) [(0::Int)..] $ reverse [
   insturl    = "",
   title      = "Continuation Passing Style",
   abstract   =
-     "<p>I chose to go through (parts of) Hatcliff and Danvy's paper <a href='http://dl.acm.org/citation.cfm?id=178053'> \"A Generic Account of Continuation-Passing Styles\"</a> (POPL 94) which gives a nice factorization of various CPS transforms in terms of:\
+     "I chose to go through (parts of) Hatcliff and Danvy's paper <a href='http://dl.acm.org/citation.cfm?id=178053'> \"A Generic Account of Continuation-Passing Styles\"</a> (POPL 94) which gives a nice factorization of various CPS transforms in terms of:\
     \  <ul>\
     \   <li>embeddings from STLC to Moggi's computational meta-language\
     \       (either call-by-value, call-by-name, or whatever you can come\
     \       up with)</li>\
     \   <li>followed by a generic CPS transform transporting terms from ML\
     \       back to STLC</li>\
-    \ </ul></p>\
-    \ <p>Here is <a href='http://gallais.org/code/GenericCPS/definitions.html'>an Agda file</a> containing what we had the time to see.</p>",
+    \ </ul>\
+    \ Here is <a href='http://gallais.org/code/GenericCPS/definitions.html'>an Agda file</a> containing what we had the time to see.",
   location   = "LT1310",
   material   = [] }
  ]
